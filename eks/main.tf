8621c09a5da2091ee4beb93e501bdf643dcf2698
Junnygram <junnexclusive@gmail.com>
2026-01-18 23:11:26 +0100

    Removed the cleanup script, added new image files, and configured internal ingress in `main.tf`.

--- eks/main.tf
+++ eks/main.tf
@@ -104,6 +104,13 @@ resource "aws_security_group" "k8s_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
